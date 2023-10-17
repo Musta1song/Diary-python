4f86ddb49512bc185c5c2e06cb5067d7cbdab621
Musta1song <147011583+Musta1song@users.noreply.github.com>
2023-10-16 21:34:57 -0500

    Add files via upload

--- Tagebuch/Tagebuch.py
+++ Tagebuch/Tagebuch.py
@@ -7,6 +7,7 @@ import subprocess
 import os
 from tkinter.filedialog import askopenfilename
 from tkinter import *
+from tkinter import scrolledtext
 
 
 currTime = datetime.now()
@@ -18,7 +19,7 @@ def speichern():
       newpath = "C:/ProgramData/Tagebucheintraege"
       if not os.path.exists(newpath):
        os.makedirs(newpath)
-      lbenter = etEingabe.get(1.0)
+      lbenter = etEingabe.get(1.0, END)
       path= "C:/ProgramData/Tagebucheintraege"
       f = ("/"+day+"-"+month+ "-" + year)
       g= ".txt"
@@ -40,7 +41,7 @@ fenster.title("Tagebuch")
 fenster.resizable(False, False)
 fenster.columnconfigure(0, weight=1)
 fenster.rowconfigure(0, weight=1)
-img = PhotoImage(file="C:\ProgramData\Tagebucheintraege\diary.png")
+img = PhotoImage(file="diary.png")
 canvas = Canvas(fenster, width = 120, height = 50)   
 canvas.create_image(500,400,image=img)
 canvas.grid(column=0,padx=0,pady=0,sticky='nsew')  
@@ -53,7 +54,7 @@ canvas.rowconfigure(0, weight=1)
 
 etEingabe = tkinter.Text(fenster,  width =100, height =35, bg="light blue")
 etEingabe.grid(row=0,column=0,padx=50,pady=0,sticky='news')
-etEingabe.configure(font=14)
+etEingabe.configure(font=(14))
 etEingabe.insert(1.9,(day+"."+month+ "." + year +"\n \n"))
 
 etEingabe.insert(3.0,"Liebes Tagebuch, \n \n ")
